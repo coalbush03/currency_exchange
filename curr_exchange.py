742827a03d62b803d2d3cea6625166daf17981aa
coalbush03 <143535354+coalbush03@users.noreply.github.com>
2024-04-04 00:08:34 +0200

    dzialajaca konwersja

--- curr_exchange.py
+++ curr_exchange.py
@@ -11,14 +11,22 @@ class curr_converter(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        curr_data, curr_list = self.load_api() #curr data do converta
-
+        curr_data, curr_list = self.load_api() 
+        self.curr_data = curr_data #curr data do converta
 
         lists = self.lists(curr_list)
         inputs = self.input_fields()
 
         self.layout.addLayout(lists) #dodanie listy i pol na inputy
         self.layout.addLayout(inputs)
+
+        self.output_field = inputs.itemAt(1).widget() #dostep do pól
+        self.input_field = inputs.itemAt(0).widget()
+        self.from_list = lists.itemAt(0).widget()
+        self.to_list = lists.itemAt(1).widget()
+
+        self.input_field.textChanged.connect(self.convert) #polaczenie pola z convertem
+
 
     def lists(self, curr_list):  #listy horyznotalnie 
         from_list = QComboBox() #drop down
@@ -47,8 +55,24 @@ class curr_converter(QWidget):
         layout.addWidget(output_f)
         return layout
 
-    def convert(self, curr_data, from_curr, to_curr, n):  #convert
-        pass
+    def convert(self): 
+        from_curr=self.from_list.currentText() #listy
+        to_curr=self.to_list.currentText() 
+        amount=self.input_field.text() #pobranie wartosci z inputu
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            self.output_field.setText("BŁĘDNY FORMAT")
+            return
+        if from_curr == to_curr:
+            self.output_field.setText(str(amount)) #bez obliczania dla tych samych walut
+            return
+        
+        from_rate = self.curr_data[from_curr] #obliczanie
+        to_rate = self.curr_data[to_curr]
+        result = amount / from_rate * to_rate
+        self.output_field.setText(str(result)) #wstawienie w output
 
     def load_api(self):
         url = 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_ybAM6h8asWboJSTGOSUy9TLPda85XRCXKRst6SoX' #wczytanie danych z api
@@ -56,10 +80,6 @@ class curr_converter(QWidget):
         curr_data = response.json()['data']
         curr_list=list(curr_data.keys())
         return curr_data, curr_list
-
-
-
-
 
 
 
